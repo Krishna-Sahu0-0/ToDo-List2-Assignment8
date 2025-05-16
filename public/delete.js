fda48ad96352285da97003065fd84a464504e03a
Krishna Sahu <24a55a4205@adityatekkali.edu.in>
2025-05-16 19:38:02 +0530

    Adjusted delete command

--- public/delete.js
+++ public/delete.js
@@ -8,3 +8,16 @@ document.querySelectorAll('.delete-btn').forEach(btn => {
             });
     });
 });
+
+document.querySelectorAll('.delete-checkbox').forEach(box => {
+    box.addEventListener('change', function () {
+        if (this.checked) {
+            const taskId = this.dataset.id;
+            fetch(`/delete/${taskId}`, { method: 'DELETE' })
+                .then(res => {
+                    if (res.ok) location.reload();
+                    else alert('Delete failed');
+                });
+        }
+    });
+});
